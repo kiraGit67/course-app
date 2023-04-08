67eb5f54745eb397c370f63375cd78c551a17cc4
kiraGit67 <kirsten@sais.de>
2023-04-08 14:31:06 +0200

    adds CoursesView

---
+++ src/views/CoursesView.vue
@@ -0,0 +1,25 @@
+<template>
+  <course-items :headline="headline" :courseList="courses" />
+</template>
+
+<script>
+import CourseItems from "@/components/CourseItems.vue";
+
+export default {
+  name: "CoursesView",
+  components: {
+    CourseItems,
+  },
+  data() {
+    return {
+      headline: "Übersicht über alle Kurse",
+      courses: [],
+    };
+  },
+  async created() {
+    const response = await fetch("http://localhost:3000/courses");
+    this.courses = await response.json();
+    console.log(this.courses);
+  },
+};
+</script>
